a2a9fe57228d0d0b6ce69e3101354ad41072c862
Dominik Mayer <dominik.mayer@gmail.com>
2016-02-23 23:41:48 +0100

    Beautified output

--- lp-process.sh
+++ lp-process.sh
@@ -2,21 +2,24 @@
 
 VIDEOS=0
 LIVE_PHOTOS=0
+PHOTOS=0
+
+echo
+echo "Renaming Live Photos"
+echo "===================="
+echo
 
 for FILE in IMG_*.MOV; do
   BASE=${FILE%.MOV}
   if [[ -e $BASE.MOV && -e $BASE.JPG ]]; then
-    echo
-    echo "Live Photo: $BASE"
     NAME=`jhead -n"%Y-%m-%d - %H-%M-%S" "$BASE.JPG"`
     echo "$NAME"
-    NAME=${NAME#IMG_*.JPG --> }
-    NAME=${NAME%.jpg}
-    echo "New Name: $NAME"
 
     # Rename video. Don't overwrite existing.
+    NAME=${NAME#IMG_*.JPG --> }
+    NAME=${NAME%.jpg}
     mv -n "$BASE.MOV" "$NAME.mov"
-    
+
     LIVE_PHOTOS=$((LIVE_PHOTOS+1))
   fi
 done
@@ -25,6 +28,11 @@ for FILE in IMG_*.MOV; do
     VIDEOS=$((VIDEOS+1))
 done
 
+for FILE in IMG_*.JPG; do
+    PHOTOS=$((PHOTOS+1))
+done
+
 echo
-echo "$VIDEOS videos"
 echo "$LIVE_PHOTOS live photos"
+echo "$PHOTOS photos"
+echo "$VIDEOS videos"
