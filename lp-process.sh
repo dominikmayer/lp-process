5560a810ae78422db7fa7415bd8596a811f68d82
Dominik Mayer <dominik.mayer@gmail.com>
2016-02-23 23:22:11 +0100

    Added renaming of video part

--- lp-process.sh
+++ lp-process.sh
@@ -6,13 +6,17 @@ LIVE_PHOTOS=0
 for FILE in IMG_*.MOV; do
   BASE=${FILE%.MOV}
   if [[ -e $BASE.MOV && -e $BASE.JPG ]]; then
+    echo
     echo "Live Photo: $BASE"
     NAME=`jhead -n"%Y-%m-%d - %H-%M-%S" "$BASE.JPG"`
-    echo "New Name: $NAME"
+    echo "$NAME"
     NAME=${NAME#IMG_*.JPG --> }
-    echo "New Name: $NAME"
     NAME=${NAME%.jpg}
     echo "New Name: $NAME"
+
+    # Rename video. Don't overwrite existing.
+    mv -n "$BASE.MOV" "$NAME.mov"
+    
     LIVE_PHOTOS=$((LIVE_PHOTOS+1))
   fi
 done
@@ -21,6 +25,6 @@ for FILE in IMG_*.MOV; do
     VIDEOS=$((VIDEOS+1))
 done
 
-
+echo
 echo "$VIDEOS videos"
 echo "$LIVE_PHOTOS live photos"
